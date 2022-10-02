ad2582fee05caad7d565e8bab2abbb9726bf4b98
mcworkaholic <94456069+mcworkaholic@users.noreply.github.com>
2022-10-01 20:11:10 -0500

    modified HTML template, styles, and routes
    added modified HTML template from colorlib as well as the needed styles
    and then modified routes.py to reflect those changes

--- routes.py
+++ routes.py
@@ -1,8 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests, json
+
 app = Flask(__name__)
 
 # two decorators, same function
-@app.route('/')
+@app.route('/', methods =["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+       # getting input with name = "name" in HTML form
+       vin = request.form.get("vin")
+       tire_pressure = request.form.get("tire_pressure")
+       first_name = request.form.get("first_name")
+       last_name = request.form.get("last_name")
+       email = request.form.get("email")
+       phone_number = request.form.get("phone_number")
+
+       url = f'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/{vin}?format=json'
+       r = requests.get(url)
+       parsed = json.loads(r.text);		
+       pretty_json = json.dumps(parsed, indent =4)
+       return "Your vehicle info:  "+ pretty_json 
+    return render_template("start.html")
+
+# def start():
+#     return render_template('start.html', the_title='Mechanic-Auto-Invoice')
+    
 @app.route('/index.html')
 def index():
     return render_template('index.html', the_title='Tiger Home Page')
